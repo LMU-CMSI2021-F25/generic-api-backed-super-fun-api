235cdfe3eaf414346b32c40cea4b242ca1bf4cfe
DanPlayz0 <damuffin36@gmail.com>
2025-10-08 23:08:27 -0700

    Following DRY principles

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,26 @@
 const API_BASE_URL = 'https://lsb-api.compiles.me';
 // const API_BASE_URL = 'http://localhost:8001';
 
+type EndpointString = `/${string}`;
+
+function fetchUpstream<T=any>(endpoint: EndpointString) {
+  return new Promise<T>((resolve, reject) => {
+    if (force_fail) reject('Failed to fetch upstream');
+    try {
+      fetch(`${API_BASE_URL}${endpoint}`, {
+        cache: 'no-cache',
+      })
+        .then((response) => {
+          if (response.status === 200) return resolve(response.json() as Promise<T>);
+          else return reject('Failed to fetch upstream');
+        })
+        .catch((error) => reject(error));
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
 export interface Snapshot {
   device_id: number;
   timestamp: string;
@@ -16,20 +36,7 @@ export interface LatestSnapshot extends Snapshot {
 const force_fail = false;
 
 export function fetchLatestSnapshots({ include_faculty_parking = true } = {}) {
-  return new Promise<LatestSnapshot[]>((resolve, reject) => {
-    if (force_fail) reject('Failed to fetch latest snapshots');
-    fetch(`${API_BASE_URL}/snapshots?include_faculty_parking=${include_faculty_parking}`, {
-      cache: 'no-cache',
-    })
-      .then((response) => {
-        if (response.status === 200)
-          return resolve(
-            response.json().then((r) => r.filter((snap: LatestSnapshot) => snap.name.startsWith('P'))) as Promise<LatestSnapshot[]>
-          );
-        else return reject('Failed to fetch latest snapshots');
-      })
-      .catch((error) => reject(error));
-  });
+  return fetchUpstream<LatestSnapshot[]>(`/snapshots?include_faculty_parking=${include_faculty_parking}`);
 }
 
 export interface AllTimeWeeklyHourlyAverage {
@@ -40,17 +47,7 @@ export interface AllTimeWeeklyHourlyAverage {
 }
 
 export function fetchWeeklyHourlyAverages() {
-  return new Promise<AllTimeWeeklyHourlyAverage[]>((resolve, reject) => {
-    if (force_fail) reject('Failed to fetch latest snapshots');
-    fetch(`${API_BASE_URL}/weekly-hourly/alltime`, {
-      cache: 'no-cache',
-    })
-      .then((response) => {
-        if (response.status === 200) return resolve(response.json() as Promise<AllTimeWeeklyHourlyAverage[]>);
-        else return reject('Failed to fetch weekly hourly averages');
-      })
-      .catch((error) => reject(error));
-  });
+  return fetchUpstream<AllTimeWeeklyHourlyAverage[]>(`/weekly-hourly/alltime`);
 }
 
 export interface ThisWeeklyHourlyAverage {
@@ -62,29 +59,9 @@ export interface ThisWeeklyHourlyAverage {
 }
 
 export function fetchThisWeeklyHourlyAverages() {
-  return new Promise<ThisWeeklyHourlyAverage[]>((resolve, reject) => {
-    if (force_fail) reject('Failed to fetch latest snapshots');
-    fetch(`${API_BASE_URL}/weekly-hourly/thisweek`, {
-      cache: 'no-cache',
-    })
-      .then((response) => {
-        if (response.status === 200) return resolve(response.json() as Promise<ThisWeeklyHourlyAverage[]>);
-        else return reject('Failed to fetch this weekly hourly averages');
-      })
-      .catch((error) => reject(error));
-  });
+  return fetchUpstream<ThisWeeklyHourlyAverage[]>(`/weekly-hourly/thisweek`);
 }
 
 export function fetchSnapshotHistory(device_id: number) {
-  return new Promise<Snapshot[]>((resolve, reject) => {
-    if (force_fail) reject('Failed to fetch latest snapshots');
-    fetch(`${API_BASE_URL}/snapshots/${device_id}`, {
-      cache: 'no-cache',
-    })
-      .then((response) => {
-        if (response.status === 200) return resolve(response.json() as Promise<Snapshot[]>);
-        else return reject('Failed to fetch snapshot history');
-      })
-      .catch((error) => reject(error));
-  });
+  return fetchUpstream<Snapshot[]>(`/snapshots/${device_id}`);
 }
